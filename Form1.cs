bd873c254f8eb57839c9a55ccf9b1e903c2750e4
Maxim Belyaev <70077079+ameteeth@users.noreply.github.com>
2020-10-03 17:15:23 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace _2laba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.KeyPress += (sender, e) => e.Handled = true;
+            dataGridView1.ReadOnly = true;
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar) == false) return;
+            if (e.KeyChar == Convert.ToChar(Keys.Back)) return;
+            e.Handled = true;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text, comboBox1.Text);
+            button3.Enabled = true;
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar) == true) return;
+            if (e.KeyChar == Convert.ToChar(Keys.Back)) return;
+            e.Handled = true;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.ReadOnly = false;
+            button4.Visible  = true;
+            button2.Visible  = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.ReadOnly = true;
+            button4.Visible = false;
+            button2.Visible = true; 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int ind = dataGridView1.SelectedCells[0].RowIndex;
+            dataGridView1.Rows.RemoveAt(ind);
+        }
+
+
+        
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+         
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream mystr = null;
+            if(openFileDialog1.ShowDialog () == DialogResult.OK)
+            {
+                if((mystr = openFileDialog1.OpenFile()) != null)
+                {
+                    StreamReader myread = new StreamReader(mystr);
+                    string[] str;
+                    int num;
+                    try
+                    {
+
+                        string[] str1 = myread.ReadToEnd().Split('\n');
+                        num = str1.Count();
+                        dataGridView1.RowCount = num;
+                        for ( int i = 0;i<num; i++)
+                        {
+                            str = str1[i].Split('^');
+                            for(int j = 0;j<dataGridView1.ColumnCount;j++)
+                            {
+                                try
+                                {
+                                    dataGridView1.Rows[i].Cells[j].Value = str[j];
+                                }
+                                catch
+                                {
+
+                                }
+                            }
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally
+                    {
+                        myread.Close();
+                    }
+                }
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовый документ (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(saveFileDialog.FileName, false, Encoding.Unicode);
+                try
+                {
+                    List<int> col_n = new List<int>();
+                    foreach (DataGridViewColumn col in dataGridView1.Columns)
+                        if (col.Visible)
+                        {
+                            col_n.Add(col.Index);
+                        }
+                    int x = dataGridView1.RowCount;
+                    if (dataGridView1.AllowUserToAddRows) x--;
+
+                    for (int i = 0; i < x; i++)
+                    {
+                        for (int y = 0; y < col_n.Count; y++)
+                            sw.Write(dataGridView1[col_n[y], i].Value + "^");
+                        sw.Write(" \r\n");
+                    }
+                    sw.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                }
+            }
+        }
+        private void файлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ MathB.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathaMatha
+{
+    public class MathL
+    {
+        public static double add(double Firstvalue, double Secondvalue)
+        {
+            return Firstvalue + Secondvalue;
+        }
+        public static double diff(double Firstvalue, double Secondvalue)
+        {
+            return Firstvalue - Secondvalue;
+        }
+        public static double multiply(double Firstvalue, double Secondvalue)
+        {
+            return Firstvalue * Secondvalue;
+        }
+        public static double division(double Firstvalue, double Secondvalue)
+        {
+            if (Secondvalue == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Firstvalue / Secondvalue;
+            }
+        }
+        public static double Circle(double Radius)
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public static double Square(double Side)
+        {
+            return Math.Pow(Side, 2);
+        }
+        public static double areaParallelogram(double Footing, double Height)
+        {
+            return Footing * Height;
+        }
+    }
+}
